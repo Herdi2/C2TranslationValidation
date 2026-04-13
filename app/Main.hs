4a383f7d8f920def16b961b14ccd082785fc652f
Herdi <herdisaleh02@gmail.com>
2026-04-13 20:27:39 +0200

    Add CSV file with result of campaign

--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,21 @@ import Control.Exception (SomeException, try)
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.IORef
+import Data.List (intercalate)
 import qualified Data.Map as M
 import Data.Maybe
 import Data.SBV
 import Data.SBV.Control
+import Data.Time
 import Data.Time.Clock
 import Options.Applicative
+import System.Clock
 import System.Directory
 import System.Exit
+import System.FilePath
 import System.Random
 import Utils
+import Verifier.ErrorHandler
 
 -- Main
 
@@ -90,57 +95,83 @@ fuzz opts =
 campaign :: FilePath -> CampaignOpts -> IO ()
 campaign javaBin opts =
   do
+    -- Within the chosen directory, we create a directory
+    -- with current date and time which will hold the results
     dir <- makeAbsolute $ campaignDir opts
-    createDirectoryIfMissing True dir
-    setCurrentDirectory dir
-    let logfile = "campaign.log"
-    replicateM_ (campaignNum opts) $
+    currDate <- getDate
+    let outputDir = dir </> currDate
+    createDirectoryIfMissing True outputDir
+    setCurrentDirectory outputDir
+
+    -- The log file will store all information about the campaign
+    -- run in the following CSV headers:
+    -- Time-stamp, seed, SMT result/error type, error message, SMT time taken, total time taken
+    let csvHeaders = "time-stamp,seed,smt-result,error-message,smt-time,total-time\n"
+        logfile = "campaign.csv"
+    appendFile logfile csvHeaders
+    -- testCount is used to name the files.
+    -- Each file will get the name Test{testCount}.java
+    forM_ [1 .. campaignNum opts] $ \testCount ->
       do
-        seed <- randomIO
-        timingRef <- newIORef 0
-        let className = "Klass" <> show seed
-            resFile = dir <> className <> ".smt"
-            javafile = dir <> className <> ".java"
-            campaignConfig = mkConfig (Just resFile) (campaignZ3Timeout opts) timingRef
+        seed <- randomIO -- Fuzzer seed is randomly chosen
+        -- We measure two times
+        -- 1. Time taken to solve the SMT formula, given by SBV (Z3)
+        -- 2. Total time taken to construct and solve SMT formula
+        smtTiming <- newIORef 0
+        totalTiming <- newIORef 0
+        let className = "Test" <> show testCount
+            resFile = className <> ".smt"
+            javafile = className <> ".java"
+            campaignConfig = mkConfig (Just resFile) (campaignZ3Timeout opts) smtTiming
+        currTime <- getCurrentTime
         satRes <-
           runErrorM
             ( do
-                -- liftIO $ putStrLn $ "Generating Java file with seed: " <> show seed
                 prog <- fuzzProgram seed className
                 liftIO $ writeFile javafile prog
-                -- Method name "method" hardcoded by fuzzer, ugly
-                -- liftIO $ putStrLn $ "Compiling file " <> className <> ".java"
+                -- NOTE: Fuzzer always generates methods with name "method"
+                start <- liftIO $ getTime Monotonic
                 xmlContent <- compileJavaProgram javaBin javafile "method"
-                -- liftIO $ putStrLn $ "Verifying file " <> className <> ".java"
-                verifyXML campaignConfig xmlContent
-                -- liftIO $ putStrLn $ "SMT written to " <> resFile
+                res <- verifyXML campaignConfig xmlContent
+                end <- liftIO $ getTime Monotonic
+                liftIO $ writeIORef totalTiming (fromIntegral (toNanoSecs (diffTimeSpec end start)) / 1e9)
+                return res
             )
-        -- Output written in a format that is easy to parse
+        -- Output written in a format that is easy to parse 14725905904395840929
         -- [currTime] <seed> Error <err message>
         -- [currTime] <seed> Unsat <time taken>
         -- [currTime] <seed> Sat <model> <time taken>
-        currTime <- getCurrentTime
-        timetaken <- readIORef timingRef
-        let prefix = "[" <> show currTime <> "] " <> show seed <> " "
-        let logoutput =
+        smtTime <- readIORef smtTiming
+        totalTime <- readIORef totalTiming
+        let prefix = "[" <> show currTime <> "] " <> javafile <> " "
+        let (smtResult, errorMessage) =
               case satRes of
-                Left err -> prefix <> show err
+                Left err -> (show (_errorType err), show (_errorMessage err))
                 Right (SatResult res) ->
                   case res of
-                    (Satisfiable {}) ->
-                      prefix <> "Sat " <> show (M.toList $ getModelDictionary res) <> " " <> show timetaken
+                    (Satisfiable model _) ->
+                      ("Sat", show model)
                     (Unsatisfiable {}) ->
-                      prefix <> "Unsat " <> show timetaken
+                      ("Unsat", "")
                     (Unknown _ reason) ->
-                      prefix <> "Error " <> show timetaken <> " " <> show reason
-                    _ -> prefix <> "Error, unsupported SMTResult"
-        appendFile logfile (logoutput <> "\n")
-        deleteIfExists javafile
+                      ("Unknown", show reason)
+                    _ -> ("Error", "Unsupported SMT result")
+            logOutput = intercalate "," [show currTime, show seed, smtResult, errorMessage, show smtTime, show totalTime]
+        -- deleteIfExists javafile
+        appendFile logfile (logOutput <> "\n")
         deleteIfExists resFile
         deleteIfExists (className <> ".xml")
-        putStrLn logoutput
+        putStrLn $ prefix <> intercalate " " [smtResult, errorMessage, show smtTime, show totalTime]
 
 deleteIfExists :: FilePath -> IO ()
 deleteIfExists path = do
   exists <- doesFileExist path
   when exists $ removeFile path
+
+getDate :: IO String
+getDate =
+  do
+    now <- getCurrentTime
+    tz <- getCurrentTimeZone
+    let local = utcToLocalTime tz now
+    return $ formatTime defaultTimeLocale "%Y-%m-%d#%H:%M:%S" local
